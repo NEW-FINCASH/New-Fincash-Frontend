a37be2916dd13f5c8fbba7e7f9caae9d8d53e445
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-04-16 20:45:18 -0300

    feat: add Account page and update routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import CreateAccount from './pages/CreateAccount'
 import ConnectAccount from './pages/ConnectAccount';
 import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
+import Account from './pages/Account';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/connectaccount" element={<ConnectAccount />} />
           <Route path="/forgotpassword" element={<ForgotPassword />} />
           <Route path="/" element={<Home />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </Router>
     </>
---
+++ src/pages/Account.tsx
@@ -0,0 +1,9 @@
+export default function Account() {
+
+    return (
+        <div>
+        <h1>Account</h1>
+        <p>Welcome to the Account page!</p>
+        </div>
+    );
+}
