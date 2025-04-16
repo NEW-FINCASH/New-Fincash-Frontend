289a814ccb9606fd7ee5b87c6e808e5c17b0d32a
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-04-16 20:40:40 -0300

    feat: add Create Account page and integrate react-router-dom

---
+++ src/pages/CreateAccount.tsx
@@ -0,0 +1,9 @@
+export default function CreateAccount() {
+
+    return (
+        <div>
+        <h1>Create Account</h1>
+        <p>Welcome to the Create Account page!</p>
+        </div>
+    );
+}
