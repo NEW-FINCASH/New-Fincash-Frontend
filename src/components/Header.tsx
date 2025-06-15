7f3471f5b02fa7efac2046d9dab3e7f4b7143529
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-06-15 03:23:14 -0300

    style: adjust Header component layout for improved spacing and button styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,12 @@ export default function Header() {
         <header className="vw-100 py-3 px-5 d-flex justify-content-center align-items-center text-white" style={{ height: '80px'}}>
             <div className="d-flex justify-content-between align-items-center w-100">
                 <img src="/assets/fincash.png" alt="Logo" style={{ width: '10%'}}/>
-                <div className="d-flex justify-content-between align-items-center gap-5">
+                <div className="d-flex justify-content-between align-items-center gap-4">
                     <Link to="" className="text-white text-decoration-none">Product</Link>
                     <Link to="" className="text-white text-decoration-none">About us</Link>
                     <Link to="" className="text-white text-decoration-none">Roadmaps</Link>
                     <Link to="" className="text-white text-decoration-none">Register</Link>
-                    <Link to="" className="text-decoration-none btn btn-light rounded-5 px-4">Login</Link>
+                    <Link to="" className="text-decoration-none btn btn-light rounded-3 px-4">Login</Link>
                 </div>
             </div>
         </header>
