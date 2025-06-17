875df37a9234ee448916e8a0e781b445458dec91
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-06-17 19:07:35 -0300

    style: add notification section to Account page for improved user engagement

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -5,6 +5,15 @@ import HeaderProfile from "../components/HeaderProfile";
 import { Link } from "react-router-dom";
 
 export default function Account() {
+    const notificacoes = [
+        {
+            id: 1,
+            titulo: "Transferência realizada",
+            hora: "10:30 AM",
+            descricao: "Você realizou uma transferência de R$ 129,90,00 para LTDA Cotton Company",
+        }
+    ]
+
     const [user, setUser] = useState<User | null>(null);
 
     useEffect(() => {
@@ -29,34 +38,44 @@ export default function Account() {
                 <h1>Background Wallpaper.</h1>  
               </div>
               <div className="d-flex justify-content-center align-items-center w-100 p-4" >
-                <div className="d-flex flex-column justify-content-start align-items-start w-75 p-2">
-                    <div className="d-flex justify-content-start align-items-center">
-                        <img 
-                        src={user.photoURL ?? ""} 
-                        alt={user.displayName ?? "Usuário"}
-                        className="rounded-4 me-3"
-                        style={{ width: "80px", height: "80px", objectFit: "cover" }}
-                        />
-                        
-                        <h1 className="ml-3 text-white fw-light fs-2">{user.displayName ?? "Usuário"}</h1>
+                <div className="d-flex flex-column justify-content-start align-items-start w-50 p-2">
+                    <div className="d-flex justify-content-between align-items-start w-100">
+                        <div className="d-flex align-items-center">
+                            <img 
+                            src={user.photoURL ?? ""} 
+                            alt={user.displayName ?? "Usuário"}
+                            className="rounded-4 me-3"
+                            style={{ width: "80px", height: "80px", objectFit: "cover" }}
+                            />
+                            <h1 className="ml-3 text-white fw-light fs-2">{user.displayName ?? "Usuário"}</h1>
+                        </div>
+                        <button className="btn btn-light">Editar perfil<i className=""></i></button>
                     </div>
-                    <h2></h2>
+                    <div className="d-flex justify-content-between align-items-start mt-2 w-100">
+                        <div className="d-flex flex-column justify-content-center align-items-center w-50 mt-3">
+                            <input className="w-100 mb-4 bg-dark py-1 px-2 rounded border-0 text-white fw-light" type="" placeholder="thomazmelo1912@gmail.com" />
+                            <input className="w-100 mb-4 bg-dark py-1 px-2 rounded border-0 text-white fw-light" type="" placeholder="Av. Elisio Galdino Sobrinho, São José Dos Ca..."/>
+                            <input className="w-100 mb-4 bg-dark py-1 px-2 rounded border-0 text-white fw-light" type="" placeholder="+55 (12) 98707-2463"/>
+                            <input className="w-100 mb-4 bg-dark py-1 px-2 rounded border-0 text-white fw-light" type="" placeholder="@thomaz_mellux" />
+                        </div>
+                        <div className="w-50 ps-3">
+                            <div className="bg-dark w-100 mt-3 rounded p-3">
+                                <h1 className="text-white fs-4 fw-medium text-center">Ultimas notificações</h1>
+                                {notificacoes.map((notificacao) => (
+                                    <div key={notificacao.id} className="px-3 py-2 text-white border w-100 mt-3 rounded p-1">
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <h1 className="fw-medium fs-5 m-0">{notificacao.titulo}</h1>
+                                            <p className="text-light-emphasis m-0" style={{ fontSize: "12px" }}>{notificacao.hora}</p>
+                                        </div>
+                                        <p className="m-0 text-secondary" style={{ fontSize: "15px" }}>{notificacao.descricao}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
                 </div>
               </div>
             </div>
         </>
     );
 }
-
-// <div className="d-flex align-items-center gap-3 mb-3">
-//                   <img
-//                     src={user.photoURL ?? ""}
-//                     alt={user.displayName ?? "Usuário"}
-//                     className="rounded-circle"
-//                     style={{ width: "80px", height: "80px", objectFit: "cover" }}
-//                   />
-//                   <div>
-//                     <h5 className="mb-1">{user.displayName}</h5>
-//                     <p className="mb-0 email-text">{user.email}</p>
-//                   </div>
-//                 </div>
