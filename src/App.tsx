39e404e00c3b840f903db1e4761fe748055b8ddd
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-06-26 04:02:18 -0300

    style: add Plans route to App component for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
 import Account from './pages/Account';
 import Welcome from './pages/Welcome';
+import Plans from './pages/Plans';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/account" element={<Account />} />
           <Route path="/" element={<Welcome />} />
+          <Route path="/plans" element={<Plans />} />
         </Routes>
       </Router>
     </>
