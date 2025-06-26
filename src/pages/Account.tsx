283e59645c5e9987af55300f2fa095cb17a95a86
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-06-26 05:22:22 -0300

    style: add navigation link to Plans in Account component and enhance Plans page layout

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -3,6 +3,7 @@ import { auth } from "../services/firebase";
 import { useEffect, useState } from "react";
 import HeaderProfile from "../components/HeaderProfile";
 import '../App.css'
+import { Link } from "react-router-dom";
 
 export default function Account() {
     // Estado para armazenar as informações do usuário autenticado.
@@ -62,12 +63,12 @@ export default function Account() {
                             </div>
                             <div className="d-flex flex-column justify-content-between align-items-center" style={{ height: 'auto' }}>
                                 {/* Botão "Planos" adicionado */}
-                                <button className="btn btn-outline-light d-flex justify-content-between align-items-center mb-2" style={{ width: '140px' }}>
+                                <Link to="/plans" className="btn btn-outline-light d-flex justify-content-between align-items-center mb-2" style={{ width: '140px' }}>
                                     Plans
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-suit-diamond" viewBox="0 0 16 16">
                                       <path d="M8.384 1.226a.463.463 0 0 0-.768 0l-4.56 6.468a.54.54 0 0 0 0 .612l4.56 6.469a.463.463 0 0 0 .768 0l4.56-6.469a.54.54 0 0 0 0-.612zM6.848.613a1.39 1.39 0 0 1 2.304 0l4.56 6.468a1.61 1.61 0 0 1 0 1.838l-4.56 6.468a1.39 1.39 0 0 1-2.304 0L2.288 8.92a1.61 1.61 0 0 1 0-1.838z"/>
                                     </svg>
-                                </button>
+                                </Link>
                                 <button className="btn btn-outline-light d-flex justify-content-between align-items-center" style={{ width: '140px' }}>
                                     Edit profile
                                     <svg width="16" height="16" fill="currentColor" className="bi bi-pencil-square ms-2" viewBox="0 0 16 16">
